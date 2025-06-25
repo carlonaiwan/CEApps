5655e668a7dea1ac0076835934eebb3e23815401
Carlo Naiwan <carlonaiwan@gmail.com>
2025-06-25 18:37:29 +0700

    Update jadi 1 file
    penambahan scroll down

--- test_orderH2.py
+++ test_orderH2.py
@@ -5,16 +5,67 @@ from time import sleep
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
-from test_login import test_positive_login
-
 
 
 def test_positive_orderH2(driver):
+    try:
+        driver.find_element(AppiumBy.XPATH, "//android.widget.TextView[@text='Lewati']").click()
+    except NoSuchElementException:
+        print("Lewati tidak muncul, lanjut.")
 
-    test_positive_login(driver)
+    # Masukkan username dan password
+    driver.find_element(AppiumBy.XPATH,
+        "(//android.widget.EditText[@content-desc='inputFormik_id_q0sigmpf'])[1]"
+    ).send_keys("081288219696")
 
-     #scroll down kebawah sbntr ini belum nemu titik koordinat device nya :""""
+    driver.find_element(AppiumBy.XPATH,
+        "(//android.widget.EditText[@content-desc='inputFormik_id_q0sigmpf'])[2]"
+    ).send_keys("Carlo19#")
 
+    driver.find_element(AppiumBy.XPATH,
+        "//android.widget.TextView[@text='MASUK']"
+    ).click()
+
+    # Masukkan PIN jika diminta
+    try:
+        sleep(10)
+        driver.find_element(AppiumBy.XPATH,
+            "//android.widget.TextView[@text='MASUK']/parent::*"
+        ).click()
+
+        for pin in ["1", "5", "9", "3", "5", "7"]:
+            driver.find_element(AppiumBy.XPATH,
+                f"//android.widget.TextView[@text='{pin}']/parent::*"
+            ).click()
+    except NoSuchElementException:
+        print("PIN tidak diperlukan, lanjut.")
+
+    
+    try:
+        element = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((
+                AppiumBy.XPATH, 
+                "//android.widget.TextView[@text='Tutup']"
+            ))
+        )
+        element.click()
+    except TimeoutException:
+        print("Timeout: Elemen tidak ditemukan dalam waktu 20 detik atau element tidak ditemukan.")
+
+
+    try:
+        element = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((
+                AppiumBy.XPATH, 
+                "//android.widget.TextView[@text='NANTI SAJA']/parent::*"
+            ))
+        )
+        element.click()
+    except TimeoutException:
+        print("Timeout: Elemen tidak ditemukan dalam waktu 20 detik atau element tidak ditemukan.")
+
+    sleep(5)
+    driver.swipe(start_x=168, start_y=1544, end_x=168, end_y=400, duration=500)
     try:
         driver.find_element(AppiumBy.XPATH, "//android.widget.TextView[@text='BOOKING SERVIS']").click()
     except NoSuchElementException:
